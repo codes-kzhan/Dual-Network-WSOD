63fc1375555c03ea6c57d715e2bc1478c4313188
Your Name <you@example.com>
2017-04-17 14:46:55 +1000

    small bug fix

--- functions/weakly/online/weakly_online_test.m
+++ functions/weakly/online/weakly_online_test.m
@@ -1,4 +1,4 @@
-function [res, mean_loc] = weakly_online_test(confs, imdb, roidb, varargin)
+function res = weakly_online_test(confs, imdb, roidb, varargin)
 % --------------------------------------------------------
 % R-FCN implementation
 % Modified from MATLAB Faster R-CNN (https://github.com/shaoqingren/faster_rcnn)
@@ -29,210 +29,197 @@ function [res, mean_loc] = weakly_online_test(confs, imdb, roidb, varargin)
     weakly_assert_conf(confs);
     classes = confs{1}.classes;
 %%  set cache dir
-    cache_dir = fullfile(pwd, 'output', 'weakly_cachedir', opts.cache_name, [imdb.name, '_mAP']);
+    cache_dir = fullfile(pwd, 'output', 'weakly_cachedir', opts.cache_name, [imdb.name, '_online_results']);
     mkdir_if_missing(cache_dir);
 
 %%  init log
     timestamp = datestr(datevec(now()), 'yyyymmdd_HHMMSS');
     mkdir_if_missing(fullfile(cache_dir, 'log'));
-    log_file  = fullfile(cache_dir, 'log', [opts.log_prefix, 'test_', timestamp, '.txt']);
-    save_file = fullfile(cache_dir, 'log', [opts.log_prefix, 'test_', timestamp, '.mat']);
+    log_file = fullfile(cache_dir, 'log', [opts.log_prefix, 'test_', timestamp, '.txt']);
     diary(log_file);
     
     num_images = length(imdb.image_ids);
     num_classes = imdb.num_classes;
     caffe.reset_all(); 
     
-    %%% Ground Truth for corloc
-    gt_boxes = cell(num_images, 1);
-    for i = 1:num_images
-      gt = roidb.rois(i).gt;
-      Struct = struct('class', roidb.rois(i).class(gt), ...
-                      'boxes', roidb.rois(i).boxes(gt,:));
-      gt_boxes{i} = Struct;
-    end
-    
-%%  testing 
-    % init caffe net
-    caffe_log_file_base = fullfile(cache_dir, 'caffe_log');
-    caffe.init_log(caffe_log_file_base);
-    caffe_net = [];
-    for i = 1:numel(opts.net_defs)
-      caffe_net{i} = caffe.Net(opts.net_defs{i}, 'test');
-      caffe_net{i}.copy_from(opts.net_models{i});
-    end
+    try
+      aboxes = cell(num_classes, 1);
+      if opts.ignore_cache
+          throw('');
+      end
+      for i = 1:num_classes
+        load(fullfile(cache_dir, [imdb.classes{i} '_boxes_' imdb.name opts.suffix]));
+        aboxes{i} = boxes;
+      end
+    catch    
+%%      testing 
+        % init caffe net
+        caffe_log_file_base = fullfile(cache_dir, 'caffe_log');
+        caffe.init_log(caffe_log_file_base);
+        caffe_net = [];
+        for i = 1:numel(opts.net_defs)
+          caffe_net{i} = caffe.Net(opts.net_defs{i}, 'test');
+          caffe_net{i}.copy_from(opts.net_models{i});
+        end
 
-    % set random seed
-    prev_rng = seed_rand(opts.rng_seed);
-    caffe.set_random_seed(opts.rng_seed);
+        % set random seed
+        prev_rng = seed_rand(opts.rng_seed);
+        caffe.set_random_seed(opts.rng_seed);
 
-    % set gpu/cpu
-    if confs{1}.use_gpu
-      caffe.set_mode_gpu();
-    else
-      caffe.set_mode_cpu();
-    end             
+        % set gpu/cpu
+        if confs{1}.use_gpu
+            caffe.set_mode_gpu();
+        else
+            caffe.set_mode_cpu();
+        end             
 
-    % determine the maximum number of rois in testing 
+        % determine the maximum number of rois in testing 
 
-    disp('opts:');
-    disp(opts);
-    for i = 1:numel(confs)
-      fprintf('conf : %d :', i);
-      disp(confs{i});
-    end
+        disp('opts:');
+        disp(opts);
+        for i = 1:numel(confs)
+            fprintf('conf : %d :', i);
+            disp(confs{i});
+        end
         
-    %heuristic: keep an average of 160 detections per class per images prior to NMS
-    max_per_set = 160 * num_images;
-    % heuristic: keep at most 400 detection per class per image prior to NMS
-    max_per_image = 400;
-    % detection thresold for each class (this is adaptively set based on the max_per_set constraint)
-    thresh = -inf * ones(num_classes, 1);
+        %heuristic: keep an average of 160 detections per class per images prior to NMS
+        max_per_set = 160 * num_images;
+        % heuristic: keep at most 400 detection per class per image prior to NMS
+        max_per_image = 400;
+        % detection thresold for each class (this is adaptively set based on the max_per_set constraint)
+        thresh = -inf * ones(num_classes, 1);
+        % top_scores will hold one minheap of scores per class (used to enforce the max_per_set constraint)
+        top_scores = cell(num_classes, 1);
+        % all detections are collected into:
+        %    all_boxes[cls][image] = N x 5 array of detections in
+        %    (x1, y1, x2, y2, score)
+        aboxes = cell(num_classes, 1);
+        box_inds = cell(num_classes, 1);
+        for i = 1:num_classes
+            aboxes{i} = cell(length(imdb.image_ids), 1);
+            box_inds{i} = cell(length(imdb.image_ids), 1);
+        end
 
-    num_nets   = numel(opts.net_defs);
+        count = 0;
+        t_start = tic;
+        for i = 1:num_images
+            count = count + 1;
+            if (rem(count, opts.dis_itertion) == 1)
+              fprintf('%s: test (%s) %d/%d ', procid(), imdb.name, count, num_images);
+            end
+            th = tic;
+            im = imread(imdb.image_at(i));
+            pre_boxes = roidb.test_boxes{i};
+            
+            boxes  = [];
+            scores = [];
+            for jj = 1:numel(caffe_net)
+                [cboxes, cscores] = weakly_im_detect(confs{jj}, caffe_net{jj}, im, pre_boxes, confs{jj}.max_rois_num_in_gpu);
+                if (isempty(boxes) && isempty(scores))
+                    boxes  = cboxes;
+                    scores = cscores;
+                else
+                    boxes  = boxes + cboxes;
+                    scores = scores + cscores;
+                end
+            end
+            boxes  = boxes  ./ numel(caffe_net);
+            scores = scores ./ numel(caffe_net);
 
-    t_start = tic;
+            for j = 1:num_classes
+                inds = find(scores(:, j) > thresh(j));
+                if ~isempty(inds)
+                    [~, ord] = sort(scores(inds, j), 'descend');
+                    ord = ord(1:min(length(ord), max_per_image));
+                    inds = inds(ord);
+                    cls_boxes = boxes(inds, (1+(j-1)*4):((j)*4));
+                    cls_scores = scores(inds, j);
+                    aboxes{j}{i} = [aboxes{j}{i}; cat(2, single(cls_boxes), single(cls_scores))];
+                    box_inds{j}{i} = [box_inds{j}{i}; inds];
+                else
+                    aboxes{j}{i} = [aboxes{j}{i}; zeros(0, 5, 'single')];
+                    box_inds{j}{i} = box_inds{j}{i};
+                end
+            end
+            
+            if (rem(count, opts.dis_itertion) == 1)
+              fprintf(' time %.3fs\n', toc(th)); 
+            end
+            if mod(count, 1000) == 0
+                for j = 1:num_classes
+                [aboxes{j}, box_inds{j}, thresh(j)] = ...
+                    keep_top_k(aboxes{j}, box_inds{j}, i, max_per_set, thresh(j));
+                end
+                %disp(thresh);
+                diary; diary;
+            end    
+        end
 
-    aboxes_map_fusion = cell(num_classes, 1);
-    for i = 1:num_classes
-      aboxes_map_fusion{i} = cell(num_images, 1);
-    end
-    aboxes_cor_net = cell(num_images, num_nets);
-
-    for inet = 1:num_nets
-      caffe.reset_all();
-      caffe_net = caffe.Net(opts.net_defs{inet}, 'test');
-      caffe_net.copy_from(opts.net_models{inet});
-      fprintf('>>> %3d / %3d net-prototxt:: %s\n', inet, num_nets, opts.net_defs{inet});
-      fprintf('>>> %3d / %3d net-weights :: %s\n', inet, num_nets, opts.net_models{inet});
-
-      for i = 1:num_images
-        if (rem(i, opts.dis_itertion) == 1), fprintf('%s: test (%s) %d/%d cost : %.1f s\n', procid(), imdb.name, i, num_images, toc(t_start)); end
-        d = roidb.rois(i);
-        im = imread(imdb.image_at(i));
-        pre_boxes = d.boxes(~d.gt, :);
-        [boxes, scores] = weakly_im_detect(confs{inet}, caffe_net, im, pre_boxes, confs{inet}.max_rois_num_in_gpu);
-
-        cor_boxes = zeros(0, 4); 
         for j = 1:num_classes
-            cls_boxes = boxes(:, (1+(j-1)*4):((j)*4));
-            cls_scores = scores(:, j);
-            temp = cat(2, single(cls_boxes), single(cls_scores));
-            if (isempty(aboxes_map_fusion{j}{i}))
-              aboxes_map_fusion{j}{i} = temp ./ num_nets;
-            else
-              aboxes_map_fusion{j}{i} = temp ./ num_nets + aboxes_map_fusion{j}{i};
+            [aboxes{j}, box_inds{j}, thresh(j)] = ...
+                keep_top_k(aboxes{j}, box_inds{j}, i, max_per_set, thresh(j));
+        end
+        disp(thresh');
+
+        for i = 1:num_classes
+            top_scores{i} = sort(top_scores{i}, 'descend');  
+            if (length(top_scores{i}) > max_per_set)
+                thresh(i) = top_scores{i}(max_per_set);
             end
 
-            tscore = temp(:, 5); 
-            tboxes = temp(:, 1:4);
-            [~, idx] = max(tscore);
-            cor_boxes = [cor_boxes; tboxes(idx,:)];
+            % go back through and prune out detections below the found threshold
+            for j = 1:length(imdb.image_ids)
+                if ~isempty(aboxes{i}{j})
+                    I = find(aboxes{i}{j}(:,end) < thresh(i));
+                    aboxes{i}{j}(I,:) = [];
+                    box_inds{i}{j}(I,:) = [];
+                end
+            end
+
+            save_file = fullfile(cache_dir, [imdb.classes{i} '_boxes_' imdb.name opts.suffix]);
+            boxes = aboxes{i};
+            inds = box_inds{i};
+            save(save_file, 'boxes', 'inds');
+            clear boxes inds;
         end
-        aboxes_cor_net{i, inet} = cor_boxes;
-      end
-    end
-    caffe.reset_all(); 
-    rng(prev_rng);
-
-    % ------------------------------------------------------------------------
-    % Peform Corloc evaluation
-    % ------------------------------------------------------------------------
-    tic;
-    aboxes_cor = cell(num_images, 1);
-    for i = 1:num_images
-      cor_boxes = zeros(0, 4);
-      for cls = 1:num_classes
-        temp = aboxes_map_fusion{cls}{i};
-        tscore = temp(:, 5);
-        tboxes = temp(:, 1:4);
-        [~, idx] = max(tscore);
-        cor_boxes = [cor_boxes; tboxes(idx,:)];
-      end 
-      aboxes_cor{i} = cor_boxes;
-    end
-    res_cor_net = cell(num_nets, 1);
-    for inet = 1:num_nets
-      res_cor_net{inet} = corloc(num_classes, gt_boxes, aboxes_cor_net, 0.5);
-      res_cor_net{inet} = res_cor_net{inet} * 100;
+        fprintf('test all images in %f seconds.\n', toc(t_start));
+        
+        caffe.reset_all(); 
+        rng(prev_rng);
     end
 
-    [res_cor] = corloc(num_classes, gt_boxes, aboxes_cor, 0.5);
-    fprintf('\n~~~~~~~~~~~~~~~~~~~~\n');
-    fprintf('Fusion CorLoc Results:\n');
-    res_cor = res_cor * 100;
-    assert( numel(classes) == numel(res_cor));
-    for idx = 1:numel(res_cor)
-      if (num_nets == 2)
-        fprintf('%12s : corloc : %5.2f  [%5.2f , %5.2f ]\n', classes{idx}, res_cor(idx), res_cor_net{1}(idx), res_cor_net{2}(idx));
-      else
-        fprintf('%12s : corloc : %5.2f\n', classes{idx}, res_cor(idx));
-      end
-    end 
-    if (num_nets == 2)
-      fprintf('\nmean corloc : %.4f  [%.4f  , %.4f]\n', mean(res_cor), mean(res_cor_net{1}), mean(res_cor_net{2}));
-    else
-      fprintf('\nmean corloc : %.4f\n', mean(res_cor));
-    end
-    fprintf('~~~~~~~~~~~~~~~~~~~~ evaluate cost %.2f s\n', toc);
-    mean_loc = mean(res_cor);
-
-
-
     % ------------------------------------------------------------------------
-    % Peform AP evaluation
+    % Save for online evaluation
     % ------------------------------------------------------------------------
+
+	tic;
     %  only for pascal voc
-    res = cell(num_classes, 1); 
+    res = cell(num_classes, 1);
     parfor model_ind = 1:num_classes
         cls = imdb.classes{model_ind};
         res{model_ind} = prepare_online_voc(cls, aboxes{model_ind}, imdb, opts.cache_name, opts.suffix);
     end
-
     
     diary off;
 end
 
 
 % ------------------------------------------------------------------------
-function [boxes] = keep_top_k(boxes, top_k)
+function [boxes, box_inds, thresh] = keep_top_k(boxes, box_inds, end_at, top_k, thresh)
 % ------------------------------------------------------------------------
     % Keep top K
-    X = cat(1, boxes{:});
+    X = cat(1, boxes{1:end_at});
     if isempty(X)
         return;
     end
     scores = sort(X(:,end), 'descend');
     thresh = scores(min(length(scores), top_k));
-    for image_index = 1:numel(boxes)
+    for image_index = 1:end_at
         if ~isempty(boxes{image_index})
             bbox = boxes{image_index};
             keep = find(bbox(:,end) >= thresh);
             boxes{image_index} = bbox(keep,:);
+            box_inds{image_index} = box_inds{image_index}(keep);
         end
-    end
-end
-
-% ------------------------------------------------------------------------
-function [res] = corloc(num_class, gt_boxes, all_boxes, corlocThreshold)
-% ------------------------------------------------------------------------
-    num_image = numel(gt_boxes);     assert (num_image == numel(all_boxes));
-    res = zeros(num_class, 1);
-    for cls = 1:num_class
-        overlaps = [];
-        for idx = 1:num_image
-           gt = gt_boxes{idx};
-           gtboxes = gt.boxes(gt.class == cls, :);
-           if (isempty(gtboxes)), continue; end
-           localizedBox = all_boxes{idx}(cls, :);
-           overlap = iou(gtboxes, localizedBox);
-           overlap = max(overlap);
-           if (overlap >= corlocThreshold)
-             overlaps(end+1) = 1;
-           else
-             overlaps(end+1) = 0;
-           end
-        end
-        res(cls) = mean(overlaps);
     end
 end
