952953548ccf22fa63929dee96a585014380f207
DXY <Xuanyi.Dong@student.uts.edu.au>
2017-04-18 15:12:26 +1000

    small bug fix v6

--- experiments/+Dataset/voc2012_trainval_ss.m
+++ experiments/+Dataset/voc2012_trainval_ss.m
@@ -1,4 +1,4 @@
-function dataset = voc2012_trainval_ss(dataset, usage, use_flip)
+function dataset = voc2012_trainval_ss(dataset, usage, use_flip, exclude_difficult_samples)
 % Pascal voc 0712 trainval set with selective search
 % set opts.imdb_train opts.roidb_train 
 % or set opts.imdb_test opts.roidb_train
@@ -9,7 +9,7 @@ devkit2012                      = voc2012_devkit();
 switch usage
     case {'train'}
         dataset.imdb_train    = { imdb_from_voc(devkit2012, 'trainval', '2012', use_flip) };
-        dataset.roidb_train   = cellfun(@(x) x.roidb_func(x, 'with_selective_search', true), dataset.imdb_train, 'UniformOutput', false);
+        dataset.roidb_train   = cellfun(@(x) x.roidb_func(x, 'with_selective_search', true, 'exclude_difficult_samples', exclude_difficult_samples), dataset.imdb_train, 'UniformOutput', false);
     case {'test'}
         error('only supports one source test currently');  
     otherwise
